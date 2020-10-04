222f09a8a10761bdf707ec0534a5caf0db05c3d0
Chris Enitan <enitanchris@gmail.com>
2020-10-04 13:13:01 +0200

    initial

---
+++ background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function() {
+
+    console.log("The color is green.");
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'web.whatsapp.com'},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
+
+      
+  });
