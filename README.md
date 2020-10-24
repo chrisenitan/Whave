b189eb8d44077f17d578b2d35d1ca0d5e7cc8182
Chris Enitan <enitanchris@gmail.com>
2020-10-25 00:26:47 +0200

    allow fast switch and polished custom time wip

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 A time-based theme changer for WhatsApp web. 
 <br> Initial Gist at https://gist.github.com/chrisenitan/6142637e5a59cd412e15af058cde3f8f
 
+**Features** <br>
+1 Switch theme between day and night time<br>
+2 Set custom time to theme changes<br>
+3 Turn on and off theme manually<br>
+
 **Current**
-- Working version: Unpacked Chrome extension
+- Working version: Packed and unpacked Chrome extension
 
 **Planned**
 - Support custom start and end time for theme switches
--- theme.js
+++ theme.js
@@ -4,62 +4,112 @@
 
 let switchTheme = (req) =>{
 
-if(req != undefined){console.log(req)}
-var req = req
+	//get the current time
+	var time = new Date();
+	var hour = time.getHours();
+	var stampTime = `${hour}:${time.getMinutes()}`;
 
-		//get the current time 
-		var time = new Date();
-		let hour = time.getHours(); 
-		let stampTime = `${hour}:${time.getMinutes()}`;
-	
-		//This changes to dark mode from 8pm to 7am
-		if(hour >= 20 || hour <= 7){
-		document.querySelector(".web").setAttribute("class", "web dark");
-		console.log(`Dark mode triggered at ${stampTime}. ${req}`);
-		}
-		else{
+    if(req == undefined){
+        //This changes to dark mode from 8pm to 7am
+        if(hour >= 20 || hour <= 7){
+        document.querySelector(".web").setAttribute("class", "web dark");
+        console.log(`Dark mode triggered at ${stampTime}`);
+        }
+        else{
+        document.querySelector(".web").setAttribute("class", "web");
+        console.log(`Light mode triggered at ${stampTime}`);
+        }
+    }
+    else{
+       if (req.manual == "startLight") {
 		document.querySelector(".web").setAttribute("class", "web");
-		console.log(`Light mode triggered at ${stampTime}. ${req}`);
-		}	
-}
-
-
-window.addEventListener("click", function() {
-
-switchTheme()
-
-});  
-
-//get the current time 
-var time = new Date();
-let currentHour = time.getHours(); 
-
-if(currentHour >= 20 || currentHour <= 7){
-	document.getElementById('nextSwitchTime').innerHTML="Light mode will begin at 7am";
-}
-else{
-	document.getElementById('nextSwitchTime').innerHTML="Dark mode will begin at 8pm";
-}
-
-document.getElementById('nextSwitch').addEventListener("click", function(){
-
-	//considered using declarativeContent but no idea how sync works yet
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-			tabs[0].id,
-			{code: 'switchTheme("test")'});
-	  });
-
-})
-
-
-
-   
-
-
-
-
-
-
-
+		console.log(`Light mode manually triggered at ${stampTime}`);
+	   }
+	   else if(req.manual == "startDark"){
+		document.querySelector(".web").setAttribute("class", "web dark");
+		console.log(`Dark mode manually triggered at ${stampTime}`);
+	   }
+	   else{
+		   //this should not happen for now
+	   }
+    }	
+    }
+    
+    //attach function to DOM
+    window.addEventListener("click", function() {
+    switchTheme()
+    });  
 	
+	/* 
+	get the current time
+	..need to set this .nextSwitchTime in DOM	
+	*/
+    var time = new Date();
+    let currentHour = time.getHours();    
+    if(currentHour >= 20 || currentHour <= 7){
+        document.getElementById('nextSwitchTime').innerHTML="Light mode will begin at 7am";
+    }
+    else{
+        document.getElementById('nextSwitchTime').innerHTML="Dark mode will begin at 8pm";
+    }
+    
+   /*
+   document.getElementById('nextSwitch').addEventListener("click", function(){
+    
+        //considered using declarativeContent but no idea how sync works yet
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+                {code: `switchTheme("test")`});
+          });
+    
+    }) */
+    
+    
+    //start dark mode
+    document.getElementById('startDark').addEventListener("click", function(){
+    
+        //considered using declarativeContent but no idea how sync works yet
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+				{code: `
+				var req = {
+					manual:"startDark"
+				}
+				switchTheme(req)
+				`});
+          });
+    
+    })
+	
+
+    //start light mode
+    document.getElementById('startLight').addEventListener("click", function(){
+    
+        //considered using declarativeContent but no idea how sync works yet
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+                tabs[0].id,
+				{code: `
+				var req = {
+					manual:"startLight"
+				}
+				switchTheme(req)
+				`});
+          });
+    
+    })
+
+    
+    
+       
+    
+    
+    
+    
+    
+    
+    
+        
+    
